5aa7a1faf8ed219a4187a638383201df8b8c85f9
刘汉灵 <lhl@office>
2016-09-07 14:51:59 +0800

    修正处理socket屏蔽错误

--- Crontab/Network/Socket.php
+++ Crontab/Network/Socket.php
@@ -37,32 +37,44 @@ class Socket
         $address = ConfigManager::get('listen.listen_addr');
         $port = ConfigManager::get('listen.listen_port');
         
-        if(!$this->_socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP))
+        $display_errors = ini_get('display_errors');
+        ini_set('display_errors',0);
+        
+        try
         {
-            $this->_message[] = "can not create socket";
+            if(!$this->_socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP))
+            {
+                throw new \Exception("can not create socket");
+            }
+
+            if(!socket_set_option($this->_socket, SOL_SOCKET, SO_REUSEADDR, 1))
+            {
+                socket_close($this->_socket);
+                throw new \Exception("can not set socket option");
+            }
+
+            if(!socket_bind($this->_socket, $address, $port))
+            {
+                socket_close($this->_socket);
+                throw new \Exception("can not bind socket");
+            }
+
+            if(!socket_listen($this->_socket, 5))
+            {
+                socket_close($this->_socket);
+                throw new \Exception("can not listen");
+            }
+            
+        }
+        catch (\Exception $ex)
+        {
+            ini_set('display_errors',$display_errors);
+            $this->_message[] = $ex->getMessage();
             return FALSE;
         }
         
-        if(!socket_set_option($this->_socket, SOL_SOCKET, SO_REUSEADDR, 1))
-        {
-            socket_close($this->_socket);
-            $this->_message[] = "can not set socket option";
-            return FALSE;
-        }
-        
-        if(!socket_bind($this->_socket, $address, $port))
-        {
-            socket_close($this->_socket);
-            $this->_message[] = "can not bind socket";
-            return false;
-        }
-        
-        if(!socket_listen($this->_socket, 5))
-        {
-            socket_close($this->_socket);
-            $this->_message[] = "can not listen";
-            return false;
-        }
+        ini_set('display_errors',$display_errors);
+        return TRUE;
     }
     
 }
